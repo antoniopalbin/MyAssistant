f5f860acf388486821ab461b4abff2b3eaa2709e
ajcr0007 <ajcr0007@gmail.com>
2017-05-01 13:31:49 +0200

    Creada vista de registro de actividad (List personalizado en horizontal y vertical y datepickerdialog)

--- app/src/main/java/com/master/antonio/myassistant/MainActivity.java
+++ app/src/main/java/com/master/antonio/myassistant/MainActivity.java
@@ -1,14 +1,27 @@
 package com.master.antonio.myassistant;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 
 import com.activeandroid.ActiveAndroid;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+import static android.R.attr.startYear;
+
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    private DatePickerDialog datePickerDialog;
+    private Context cont;
+    private int Day;
+    private int Mounth;
+    private int Year;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +29,17 @@ public class MainActivity extends AppCompatActivity {
         //Initializing Active Android
         ActiveAndroid.initialize(this);
         setContentView(R.layout.activity_main);
+        cont = this;
+
+        //Obtenemos la fecha actual
+        final Calendar c = Calendar.getInstance();
+        int year_aux = c.get(Calendar.YEAR);
+        int month_aux = c.get(Calendar.MONTH);
+        int day_aux = c.get(Calendar.DAY_OF_MONTH);
+
+        //Configuramos el datepicker con la fecha actual
+        datePickerDialog = new DatePickerDialog(cont,android.R.style.Theme_DeviceDefault_Light_Dialog, MainActivity.this, year_aux, month_aux, day_aux);
+
 
         Button buttonAdmin = (Button) findViewById(R.id.buttonAdmin);
         Button buttonRegisterActivity = (Button) findViewById(R.id.buttonRegisterOfActivity);
@@ -32,9 +56,13 @@ public class MainActivity extends AppCompatActivity {
         {
             public void onClick(View v)
             {
-                startGestorOfActivity();
+                //startGestorOfActivity();
+                datePickerDialog.show();
             }
         });
+
+
+
     }
 
     private void  startAdministrator(){
@@ -46,5 +74,16 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
 
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        //Almacenamos la fecha seleccionada por el usuario
+        Day = dayOfMonth;
+        Mounth = month;
+        Year = year;
+
+        //Lanzamos la actividad
+        startGestorOfActivity();
     }
 }
--- app/src/main/java/com/master/antonio/myassistant/RegisterActivity.java
+++ app/src/main/java/com/master/antonio/myassistant/RegisterActivity.java
@@ -2,15 +2,119 @@ package com.master.antonio.myassistant;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by anton on 30/04/2017.
  */
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private class Register {
+        private String titulo="Sin marca";
+        private String timestamp="Sin matricula";
+        private int icono=android
+                .R
+                .mipmap
+                .sym_def_app_icon;
+
+        public Register(){}
+
+        public Register(String titulo
+                , String timestamp
+                , int icono) {
+            this.titulo = titulo;
+            this.timestamp = timestamp;
+            this.icono = icono;
+        }
+
+        public String getTitulo() {
+            return titulo;
+        }
+
+        public String getTimestamp() {
+            return timestamp;
+        }
+
+        public int getIcono() {
+            return icono;
+        }
+    }
+
+    private ListView ListRegister;
+    private ArrayList<Register> Registros = new ArrayList<Register>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Añadimos registros de prueba
+        Registros.add(new Register("Cocina","01/05/2017 12:05",R.mipmap.cocina));
+        Registros.add(new Register("Salón","01/05/2017  13:50",R.mipmap.tv));
+        Registros.add(new Register("Dormitorio","01/05/2017 14:50",R.mipmap.dormitorio));
+        Registros.add(new Register("Baño","01/05/2017 17:50",R.mipmap.bano));
+
+        showLVVehiculos_Complejo();
+    }
+
+    private void showLVVehiculos_Complejo() {
+        ListRegister=(ListView) findViewById(R.id.ListRegister);
+        ListRegister.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
+
+        ArrayAdapter adaptadorVehiculos=
+                new ArrayAdapter( this // Context
+                        , R.layout.registeritemlayout //Resource
+                        , Registros // Vector o lista
+                ) {
+                    public View getView(int position
+                            , View convertView
+                            , ViewGroup parent) {
+                        LayoutInflater inflater = (LayoutInflater) getContext()
+                                .getSystemService(getContext().LAYOUT_INFLATER_SERVICE);
+
+                        // Creamos la vista para cada fila
+                        View fila = inflater.inflate(R.layout.registeritemlayout, parent, false);
+
+                        // Creamos cada uno de los widgets que forman una fila
+                        ImageView iconoView = (ImageView) fila.findViewById(R.id.imgIcono);
+                        TextView TextTitulo = (TextView) fila.findViewById(R.id.textTitulo);
+                        TextView TextTime = (TextView) fila.findViewById(R.id.textTime);
+
+                        // Establecemos los valores que queremos que muestren los widgets
+                        Register tmpV = Registros.get(position);
+                        iconoView.setImageResource(tmpV.getIcono());
+                        TextTitulo.setText(tmpV.getTitulo());
+                        TextTime.setText(tmpV.getTimestamp());
+                        return fila;
+                    }
+                };
+
+        ListRegister.setAdapter(adaptadorVehiculos);
+
+        /*ListRegister.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                SparseBooleanArray checked = lvVehiculos.getCheckedItemPositions();
+                if (checked == null) {
+                    Toast.makeText(MainActivity.this, "Checked is null", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                String msg = "Items marcados: ";
+                for (int i = 0; i < lvVehiculos.getCount(); ++i) {
+                    msg += (checked.get(i)) ? i + ", " : "";
+                }
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });*/
     }
 }
