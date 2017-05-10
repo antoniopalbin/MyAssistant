24647650bb5aa68da51388ffe887909d2f30b5b1
ajcr0007 <ajcr0007@gmail.com>
2017-05-10 17:54:27 +0200

    Modificación de la integración de la API de YouTube

--- app/src/main/java/com/master/antonio/myassistant/VisualizarVideoActivity.java
+++ app/src/main/java/com/master/antonio/myassistant/VisualizarVideoActivity.java
@@ -3,6 +3,7 @@ package com.master.antonio.myassistant;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
@@ -17,11 +18,11 @@ import com.google.android.youtube.player.YouTubePlayerView;
  * Created by anton on 10/05/2017.
  */
 
-public class VisualizarVideoActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
+public class VisualizarVideoActivity extends AppCompatActivity{
 
     Context cont;
     public String videoId;
-    private YouTubePlayerFragment fragment;
+    private YouTubeFragment fragment;
 
     private static final int RECOVERY_DIALOG_REQUEST =1;
 
@@ -32,40 +33,13 @@ public class VisualizarVideoActivity extends YouTubeBaseActivity implements YouT
         cont = this;
 
 
-        fragment = (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.fragment_youtube_lectura);
+        fragment = (YouTubeFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_youtube_lectura);
 
         //Prueba de visualización de video
         videoId = "WkoKLmqYQUo";
+        fragment.setVideoId(videoId);
 
-        //fragment.initialize(getString(R.string.Developer_key),VisualizarVideoActivity.this);
 
     }
 
-    @Override
-    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-        if(!b){
-            youTubePlayer.cueVideo(videoId);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-       if(requestCode == RECOVERY_DIALOG_REQUEST){
-           getYouTubePlayerProvider().initialize(getString(R.string.Developer_key),VisualizarVideoActivity.this);
-       }
-    }
-
-    protected YouTubePlayer.Provider getYouTubePlayerProvider(){
-        return (YouTubePlayerView) findViewById(R.id.fragment_youtube_lectura);
-    }
-
-    @Override
-    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-        if(youTubeInitializationResult.isUserRecoverableError()){
-            youTubeInitializationResult.getErrorDialog(this,RECOVERY_DIALOG_REQUEST).show();
-        }else{
-            String errormensaje = "Se ha producido un error al inicializar el reproductor de YouTube";
-            Toast.makeText(this, errormensaje, Toast.LENGTH_SHORT).show();
-        }
-    }
 }
