8b8b6add3f3e4223da63a8d92b4a3757fa4681ae
ajcr0007 <ajcr0007@gmail.com>
2017-06-18 18:39:25 +0200

    Creado widget para la secuencia de ayuda al usuario dependiente (Creado evento para click widget y lanzamiento de actividad)

--- app/src/main/java/com/master/antonio/myassistant/activities/Miwidget.java
+++ app/src/main/java/com/master/antonio/myassistant/activities/Miwidget.java
@@ -1,19 +1,36 @@
 package com.master.antonio.myassistant.activities;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.master.antonio.myassistant.R;
 
 /**
  * Created by anton on 16/06/2017.
  */
 
 public class Miwidget  extends AppWidgetProvider {
+
     @Override
     public void onUpdate(Context context,
                          AppWidgetManager appWidgetManager,
                          int[] appWidgetIds) {
-        //Actualizar el widget
-        //...
+
+        for (int i = 0; i < appWidgetIds.length; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+
+            //Este intent deberia inciar la actividad principal de la secuencia de suaurio dependiente (cambiar VisualizarVideoActivity.class por otra nueva)
+            Intent intent = new Intent(context, VisualizarVideoActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+            views.setOnClickPendingIntent(R.id.buttonWidget, pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
     }
 }
